0fb5d396a11b521e403645b52ea6f5881dbeeab6
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2022-01-01 17:50:41 -0300

    feat: add selection sort algorithm

---
+++ src/com/gabrielmagalhaes/datastructure/selectionsort/SortLIst.java
@@ -0,0 +1,41 @@
+package com.gabrielmagalhaes.datastructure.selectionsort;
+
+import java.util.*;
+
+public class SortLIst {
+    public static void main(String[] args) {
+        Map<String, Integer> unsortedMap = initUnsortedMap();
+        Map<String, Integer> sortedMap = sortMap(unsortedMap);
+
+        printMap(sortedMap);
+    }
+
+    private static Map<String, Integer> initUnsortedMap() {
+        return new HashMap<>(){{
+            put("Gorillaz", 141);
+            put("Bo Burnham", 234);
+            put("Lil Nas X", 94);
+            put("Kendrick Lamar", 88);
+            put("David Bowie", 61);
+            put("Elton John", 111);
+        }};
+    }
+
+    private static Map<String, Integer> sortMap(Map<String, Integer> unsortedMap) {
+        List<Map.Entry<String, Integer>> list = new LinkedList<>(unsortedMap.entrySet());
+
+        list.sort(Map.Entry.<String, Integer>comparingByValue().reversed());
+
+        Map<String, Integer> result = new LinkedHashMap<>();
+
+        for (Map.Entry<String, Integer> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
+    private static void printMap(Map<String, Integer> map) {
+        map.forEach((key, value) -> System.out.println("Artist: "+key + ", Plays: " + value));
+    }
+}
